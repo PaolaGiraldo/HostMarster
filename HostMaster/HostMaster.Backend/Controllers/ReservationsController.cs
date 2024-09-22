14e4be23809683d6997aee2a1f898b7b53e03e1c
lizegira <lizegira@bancolombia.com.co>
2024-09-21 22:31:54 -0500

    Add Reservation country api controller

---
+++ HostMaster/HostMaster.Backend/Controllers/ReservationsController.cs
@@ -0,0 +1,67 @@
+﻿using HostMaster.Backend.Data;
+using HostMaster.Shared.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace HostMaster.Backend.Controllers
+{
+    [ApiController]
+    [Route("/api/[controller]")]
+    public class ReservationsController : ControllerBase
+    {
+        private readonly DataContext _context;
+
+        public ReservationsController(DataContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAsync()
+        {
+            return Ok(await _context.Reservations.ToListAsync());
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetAsync(int id)
+        {
+            var reservation = await _context.Reservations.FindAsync(id);
+            if (reservation == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(reservation);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> PostAsynd(Reservation reservation)
+        {
+            _context.Add(reservation);
+            await _context.SaveChangesAsync();
+            return Ok(reservation);
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteAsync(int id)
+        {
+            var reservation = await _context.Reservations.FindAsync(id);
+            if (reservation == null)
+            {
+                return NotFound();
+            }
+
+            _context.Remove(reservation);
+            await _context.SaveChangesAsync();
+            return NoContent();
+        }
+
+        [HttpPut]
+        public async Task<IActionResult> PutAsynd(Reservation reservation)
+        {
+            _context.Update(reservation);
+            await _context.SaveChangesAsync();
+            return NoContent();
+        }
+    }
+}

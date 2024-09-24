d5dd8aa6cf885bcb5371eb1ca0bd8f8f6cc5da74
lizegira <lizegira@bancolombia.com.co>
2024-09-24 11:05:30 -0500

    Add generics

---
+++ HostMaster/HostMaster.Backend/Controllers/ReservationsController.cs
@@ -0,0 +1,13 @@
+﻿using HostMaster.Backend.Data;
+using HostMaster.Backend.UnitsOfWork.Interfaces;
+using HostMaster.Shared.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace HostMaster.Backend.Controllers;
+
+[ApiController]
+[Route("/api/[controller]")]
+public class ReservationsController
+{
+}

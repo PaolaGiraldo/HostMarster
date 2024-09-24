bc2b170e3fd5239f07f9eab17c3ec215d94cd14d
diegoco30 <diandres@gmail.com>
2024-09-24 09:49:55 -0500

    Add Employee, Extraservice, Payment, and Reservation entities.

---
+++ HostMaster/HostMaster.Shared/Entities/Reservation.cs
@@ -0,0 +1,32 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace HostMaster.Shared.Entities;
+
+public class Reservation
+{
+    public int Id { get; set; }
+    public DateTime StartDate { get; set; }
+    public DateTime EndDate { get; set; }
+    public int NumberOfGuests { get; set; }
+
+    [Required]
+    public string State { get; set; } = null!;
+
+    public int CustomerId { get; set; }
+
+    [Required]
+    public Customer Customer { get; set; } = null!;
+
+    public int AccommodationId { get; set; }
+
+    [Required]
+    public Accommodation Accommodation { get; set; } = null!;
+
+    // Relationships
+    public ICollection<Payment>? Payments { get; set; }
+
+    public ICollection<ExtraService>? ExtraServices { get; set; }
+
+    public ICollection<Room>? Rooms { get; set; }
+}

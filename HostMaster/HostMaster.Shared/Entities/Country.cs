b3e0a42b1907a6d4210bb335b19bb815d97ff33a
lizegira <lizegira@bancolombia.com.co>
2024-09-24 14:10:05 -0500

    Add entitites

---
+++ HostMaster/HostMaster.Shared/Entities/Country.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace HostMaster.Shared.Entities;
+
+public class Country
+{
+    public int Id { get; set; }
+
+    [Required]
+    public string Name { get; set; } = null!;
+
+    // Relationships
+    public ICollection<State>? States { get; set; }
+}
---
+++ HostMaster/HostMaster.Shared/Entities/Reservation.cs
@@ -0,0 +1,38 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace HostMaster.Shared.Entities;
+
+public class Reservation
+{
+    public int Id { get; set; }
+    public DateTime StartDate { get; set; }
+    public DateTime EndDate { get; set; }
+    public int NumberOfGuests { get; set; }
+
+    [Required]
+    public string State { get; set; } = null!;
+
+    //Foreign keys
+    public int RoomId { get; set; }
+
+    [Required]
+    public Room Room { get; set; } = null!;
+
+    public int CustomerId { get; set; }
+
+    [Required]
+    public Customer Customer { get; set; } = null!;
+
+    public int AccommodationId { get; set; }
+
+    [Required]
+    public Accommodation Accommodation { get; set; } = null!;
+
+    // Relationships
+    public ICollection<Payment>? Payments { get; set; }
+
+    public ICollection<ExtraService>? ExtraServices { get; set; }
+
+    public ICollection<Room>? Rooms { get; set; }
+}

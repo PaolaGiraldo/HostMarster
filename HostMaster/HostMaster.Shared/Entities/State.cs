55645ed0c68e97b3761086324295b653671e5fea
lizegira <lizegira@bancolombia.com.co>
2024-09-24 09:17:29 -0500

    add entities

---
+++ HostMaster/HostMaster.Shared/Entities/State.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace HostMaster.Shared.Entities;
+
+public class State
+{
+    public int Id { get; set; }
+
+    [Required]
+    public string Name { get; set; } = null!;
+
+    // Foreign keys
+    public int CountryId { get; set; }
+
+    [Required]
+    public Country Country { get; set; } = null!;
+
+    // Relationships
+    public ICollection<City>? Cities { get; set; }
+}

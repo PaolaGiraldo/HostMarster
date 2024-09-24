2523c8595c8bdfbf5d1e20cdc4612b9ff423a838
caprirey <caprirey4@gmail.com>
2024-09-24 09:17:55 -0500

    First version of asigned entities

---
+++ HostMaster/HostMaster.Shared/Entities/City.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace HostMaster.Shared.Entities;
+
+public class City
+{
+    public int Id { get; set; }
+
+    [Required]
+    public string Name { get; set; } = null!;
+
+    // Foreign keys
+    public int StateId { get; set; }
+
+    public State? State { get; set; }
+
+    // Relationships
+    public ICollection<Accommodation>? Accommodations { get; set; }
+}

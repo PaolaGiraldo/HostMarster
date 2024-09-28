d8b724d5091fbfaa7371f07705d4a27b756efa63
lizegira <lizegira@bancolombia.com.co>
2024-09-28 16:58:33 -0500

    Add Seeders

---
+++ HostMaster/HostMaster.Backend/Data/SeedRoomTypes.sql
@@ -0,0 +1,5 @@
+﻿SET IDENTITY_INSERT [dbo].[RoomTypes] ON 
+INSERT [dbo].[RoomTypes] ([Id], [TypeName],[Description],[Price],[MaxGuests]) VALUES (1, N'SINGLE',N'Una cama sencilla',60.00,2)
+INSERT [dbo].[RoomTypes] ([Id], [TypeName],[Description],[Price],[MaxGuests]) VALUES (2, N'DOUBLE',N'Una cama doble',80.00,2)
+INSERT [dbo].[RoomTypes] ([Id], [TypeName],[Description],[Price],[MaxGuests]) VALUES (3, N'DOUBLE 2 BEDS',N'Dos camas dobles',80.00,4)
+SET IDENTITY_INSERT [dbo].[RoomTypes] OFF

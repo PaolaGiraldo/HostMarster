2d53d5bd25544635122e2e02d59dea97e22cc3a5
lizegira <lizegira@bancolombia.com.co>
2024-09-27 21:10:46 -0500

    Add reservation controller

---
+++ HostMaster/HostMaster.Backend/Controllers/ReservationsController.cs
@@ -0,0 +1,14 @@
+﻿using HostMaster.Backend.UnitsOfWork.Interfaces;
+using HostMaster.Shared.Entities;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HostMaster.Backend.Controllers;
+
+[ApiController]
+[Route("/api/[controller]")]
+public class ReservationsController : GenericController<Reservation>
+{
+    public ReservationsController(IGenericUnitOfWork<Reservation> unitOfWork) : base(unitOfWork)
+    {
+    }
+}

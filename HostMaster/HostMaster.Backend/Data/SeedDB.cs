093f8663171ef291cfd711f75d0155ab1c68ed39
caprirey <caprirey4@gmail.com>
2024-09-28 15:38:40 -0500

    Add basic seed for entity for rooms and roomTypes.

--- HostMaster/HostMaster.Backend/Data/SeedDB.cs
+++ HostMaster/HostMaster.Backend/Data/SeedDB.cs
@@ -15,15 +15,65 @@ public class SeedDb
     public async Task SeedAsync()
     {
         await _context.Database.EnsureCreatedAsync();
-        await CheckDbAsync();
+        await CheckCountriesDbAsync();
+        await CheckStatesDbAsync();
+        await CheckCitiesDbAsync();
+        await CheckAccommodationsDbAsync();
+        await CheckRoomsTypesDbAsync();
+        await CheckRoomsDbAsync();
         await CheckCountriesAsync();
     }
 
-    private async Task CheckDbAsync()
+    private async Task CheckCountriesDbAsync()
     {
         if (!_context.Rooms.Any())
         {
-            var SQLScript = File.ReadAllText("Data\\Seed.sql");
+            var SQLScript = File.ReadAllText("Data\\SeedCountries.sql");
+            await _context.Database.ExecuteSqlRawAsync(SQLScript);
+        }
+    }
+
+    private async Task CheckStatesDbAsync()
+    {
+        if (!_context.Rooms.Any())
+        {
+            var SQLScript = File.ReadAllText("Data\\SeedStates.sql");
+            await _context.Database.ExecuteSqlRawAsync(SQLScript);
+        }
+    }
+
+    private async Task CheckCitiesDbAsync()
+    {
+        if (!_context.Rooms.Any())
+        {
+            var SQLScript = File.ReadAllText("Data\\SeedCities.sql");
+            await _context.Database.ExecuteSqlRawAsync(SQLScript);
+        }
+    }
+
+    private async Task CheckAccommodationsDbAsync()
+    {
+        if (!_context.Rooms.Any())
+        {
+            var SQLScript = File.ReadAllText("Data\\SeedAccommodations.sql");
+            await _context.Database.ExecuteSqlRawAsync(SQLScript);
+        }
+    }
+
+    private async Task CheckRoomsDbAsync()
+    {
+        if (!_context.Rooms.Any())
+        {
+            var SQLScript = File.ReadAllText("Data\\SeedRooms.sql");
+            await _context.Database.ExecuteSqlRawAsync(SQLScript);
+        }
+    }
+
+    private async Task CheckRoomsTypesDbAsync()
+    {
+        if (!_context.RoomTypes.Any())
+        {
+            var SQLScript = File.ReadAllText("Data\\SeedRoomTypes.sql");
             await _context.Database.ExecuteSqlRawAsync(SQLScript);
         }
     }

13dfea0be3d7c079ab85017f07cbee4099d140cd
lizegira <lizegira@bancolombia.com.co>
2024-09-29 11:40:15 -0500

    Update user entity

--- HostMaster/HostMaster.Shared/Entities/User.cs
+++ HostMaster/HostMaster.Shared/Entities/User.cs
@@ -1,89 +1,43 @@
 ﻿using HostMaster.Shared.Enums;
 using HostMaster.Shared.Resources;
-using HostMaster.Shared.Resources;
 using System.ComponentModel.DataAnnotations;
 
 namespace HostMaster.Shared.Entities;
 
-public class User : IdentityUser
+public class User
 {
-	[Key]
-	public int Id { get; set; }
+    [Key]
+    public int Id { get; set; }
 
-	[Required]
-	public string Email { get; set; } = null!;
+    [Required]
+    public string Email { get; set; } = null!;
 
-	[Required]
-	[Display(Name = "DocumentType", ResourceType = typeof(Literals))]
-	public string DocumentType { get; set; } = null!;
+    [Required]
+    [Display(Name = "DocumentType", ResourceType = typeof(Literals))]
+    public string DocumentType { get; set; } = null!;
 
-	[MaxLength(20, ErrorMessage = "El campo {0} debe tener máximo {1} caractéres.")]
-	[Required(ErrorMessage = "El campo {0} es obligatorio.")]
-	[Display(Name = "Document", ResourceType = typeof(Literals))]
-	public string Document { get; set; } = null!;
-    public class User
+    [MaxLength(20, ErrorMessage = "El campo {0} debe tener máximo {1} caractéres.")]
+    [Required(ErrorMessage = "El campo {0} es obligatorio.")]
+    [Display(Name = "Document", ResourceType = typeof(Literals))]
+    public string Document { get; set; } = null!;
 
-    {
-        [Key]
-        public int Id { get; set; }
+    [MaxLength(50, ErrorMessage = "El campo {0} debe tener máximo {1} caractéres.")]
+    [Required(ErrorMessage = "El campo {0} es obligatorio.")]
+    [Display(Name = "FirstName", ResourceType = typeof(Literals))]
+    public string FirstName { get; set; } = null!;
 
-        [Required]
-        public string Email { get; set; } = null!;
+    [MaxLength(50, ErrorMessageResourceName = "MaxLength", ErrorMessageResourceType = typeof(Literals))]
+    [Required(ErrorMessageResourceName = "RequiredField", ErrorMessageResourceType = typeof(Literals))]
+    [Display(Name = "LastName", ResourceType = typeof(Literals))]
+    public string LastName { get; set; } = null!;
 
-        [Required]
-        [Display(Name = "DocumentType", ResourceType = typeof(Literals))]
-        public string DocumentType { get; set; } = null!;
+    [Display(Name = "Photo", ResourceType = typeof(Literals))]
+    public string? Photo { get; set; } = null!;
 
-        [MaxLength(20, ErrorMessage = "El campo {0} debe tener máximo {1} caractéres.")]
-        [Required(ErrorMessage = "El campo {0} es obligatorio.")]
-        [Display(Name = "Document", ResourceType = typeof(Literals))]
-        public string Document { get; set; } = null!;
+    public string? PhoneNumber { get; set; } = null!;
 
-	[MaxLength(50, ErrorMessage = "El campo {0} debe tener máximo {1} caractéres.")]
-	[Required(ErrorMessage = "El campo {0} es obligatorio.")]
-	[Display(Name = "FirstName", ResourceType = typeof(Literals))]
-	public string FirstName { get; set; } = null!;
-        [MaxLength(50, ErrorMessage = "El campo {0} debe tener máximo {1} caractéres.")]
-        [Required(ErrorMessage = "El campo {0} es obligatorio.")]
-        [Display(Name = "FirstName", ResourceType = typeof(Literals))]
-        public string FirstName { get; set; } = null!;
+    [Display(Name = "UserType", ResourceType = typeof(Literals))]
+    public UserType UserType { get; set; } = UserType.Admin;
 
-	[MaxLength(50, ErrorMessageResourceName = "MaxLength", ErrorMessageResourceType = typeof(Literals))]
-	[Required(ErrorMessageResourceName = "RequiredField", ErrorMessageResourceType = typeof(Literals))]
-	[Display(Name = "LastName", ResourceType = typeof(Literals))]
-	public string LastName { get; set; } = null!;
-        [MaxLength(50, ErrorMessageResourceName = "MaxLength", ErrorMessageResourceType = typeof(Literals))]
-        [Required(ErrorMessageResourceName = "RequiredField", ErrorMessageResourceType = typeof(Literals))]
-        [Display(Name = "LastName", ResourceType = typeof(Literals))]
-        public string LastName { get; set; } = null!;
-
-	[Display(Name = "Photo", ResourceType = typeof(Literals))]
-	public string? Photo { get; set; }
-
-	[MaxLength(200, ErrorMessage = "El campo {0} debe tener máximo {1} caractéres.")]
-	[Required(ErrorMessage = "El campo {0} es obligatorio.")]
-	[Display(Name = "UserType", ResourceType = typeof(Literals))]
-	public UserType UserType { get; set; }
-        [Display(Name = "Photo", ResourceType = typeof(Literals))]
-        public string? Photo { get; set; }
-
-        [MaxLength(200, ErrorMessage = "El campo {0} debe tener máximo {1} caractéres.")]
-        [Required(ErrorMessage = "El campo {0} es obligatorio.")]
-        [Display(Name = "UserType", ResourceType = typeof(Literals))]
-        public UserType UserType { get; set; }
-
-	public City? City { get; set; }
-
-	[Display(Name = "Ciudad")]
-	[Range(1, int.MaxValue, ErrorMessage = "Debes seleccionar una {0}.")]
-	public int CityId { get; set; }
-
-	public string FullName => $"{FirstName} {LastName}";
-}
-        [Display(Name = "Ciudad")]
-        [Range(1, int.MaxValue, ErrorMessage = "Debes seleccionar una {0}.")]
-        public int CityId { get; set; }
-
-        public string FullName => $"{FirstName} {LastName}";
-    }
+    public string FullName => $"{FirstName} {LastName}";
 }

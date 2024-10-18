5c68d7d2cd2e0e16b4068a4335de6c6b0aab82d5
diegoco30 <diandres@gmail.com>
2024-10-17 20:25:01 -0500

    Crud servicios

---
+++ HostMaster/HostMaster.Backend/Controllers/ExtraServicesController.cs
@@ -0,0 +1,15 @@
+﻿using HostMaster.Backend.UnitsOfWork.Interfaces;
+using HostMaster.Shared.Entities;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HostMaster.Backend.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ExtraServicesController : GenericController<ExtraService>
+    {
+        public ExtraServicesController(IGenericUnitOfWork<ExtraService> unitOfWork) : base(unitOfWork)
+        {
+        }
+    }
+}

344a2b36a2f92b1f5e897b7b4472ac653a158265
lizegira <lizegira@bancolombia.com.co>
2024-11-28 22:21:15 -0500

    snapshot database

---
+++ HostMaster/HostMaster.Backend/Migrations/20241129032056_functionalDataBase.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace HostMaster.Backend.Migrations
+{
+    /// <inheritdoc />
+    public partial class functionalDataBase : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Maintenances_RoomId_AccommodationId_StartDate_EndDate",
+                table: "Maintenances");
+
+            migrationBuilder.RenameColumn(
+                name: "Like",
+                table: "Opinions",
+                newName: "Positives");
+
+            migrationBuilder.RenameColumn(
+                name: "Dislike",
+                table: "Opinions",
+                newName: "Negatives");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Maintenances_RoomId",
+                table: "Maintenances",
+                column: "RoomId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Maintenances_RoomId",
+                table: "Maintenances");
+
+            migrationBuilder.RenameColumn(
+                name: "Positives",
+                table: "Opinions",
+                newName: "Like");
+
+            migrationBuilder.RenameColumn(
+                name: "Negatives",
+                table: "Opinions",
+                newName: "Dislike");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Maintenances_RoomId_AccommodationId_StartDate_EndDate",
+                table: "Maintenances",
+                columns: new[] { "RoomId", "AccommodationId", "StartDate", "EndDate" },
+                unique: true);
+        }
+    }
+}
